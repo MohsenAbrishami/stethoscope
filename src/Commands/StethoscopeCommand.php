a7705a99fdf40115b2794be16dcefdf383558661
mohsen.abrishami <mohsen.abrishami@partsoftware.com>
2022-10-15 16:50:26 +0330

    add time to log file name

--- src/Commands/StethoscopeCommand.php
+++ src/Commands/StethoscopeCommand.php
@@ -45,7 +45,7 @@ class StethoscopeCommand extends Command
      */
     public function handle()
     {
-        $file = now()->format('Y-m-d');
+        $file = now()->format('Y-m-d H-i-s');
 
         $log = '';
 
