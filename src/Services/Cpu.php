f030a44eaa505c1a983e805cebd1ef39f0eae29e
mohsen.abrishami <mohsen.abrishami@partsoftware.com>
2022-10-18 16:28:05 +0330

    add percent to cpu and memory usage

--- src/Services/Cpu.php
+++ src/Services/Cpu.php
@@ -4,11 +4,11 @@ namespace MohsenAbrishami\Stethoscope\Services;
 
 class Cpu implements ServiceInterface
 {
-    public function monitor(string $log) :string
+    public function monitor(string $log): string
     {
         $cpuUsage = exec(" grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}' ");
 
-        $message = date('H:i:s') . " ===> cpu uage:  $cpuUsage \n";
+        $message = date('H:i:s') . ' ===> cpu uage:  ' . number_format((float)$cpuUsage, 2, '.', '') . '% \n';
 
         print_r($message);
 
