3adc0ca983cd428e1028dc95706f66089e38f05c
mohsen.abrishami <mohsen.abrishami@partsoftware.com>
2022-10-29 11:08:12 +0330

    improve output data in listen and monitor command

--- src/Traits/MessageCreatorTrait.php
+++ src/Traits/MessageCreatorTrait.php
@@ -4,29 +4,34 @@ namespace MohsenAbrishami\Stethoscope\Traits;
 
 trait MessageCreatorTrait
 {
+    public function timeMessage()
+    {
+        return date('Y-m-d H:i:s');
+    }
+
     public function cpuMessage($cpuUsage)
     {
-        return date('H:i:s') . ' ===> cpu usage: ' . number_format((float)$cpuUsage, 2, '.', '');
+        return 'cpu usage ===> ' . number_format((float)$cpuUsage, 2, '.', '') . '%';
     }
 
     public function hardDiskMessage($hardDiskUsage)
     {
-        return date('H:i:s') . " ===> hard disk free space: $hardDiskUsage Byte (" .
+        return "hard disk free space ===> $hardDiskUsage Byte (" .
             number_format($hardDiskUsage / 1024 / 1024 / 1024, 2, '.', '') .  ' GB)';
     }
 
     public function memoryMessage($memoryUsage)
     {
-        return date('H:i:s') . ' ===> memory usage: ' . number_format((float)$memoryUsage, 2, '.', '');
+        return 'memory usage ===> ' . number_format((float)$memoryUsage, 2, '.', '') . '%';
     }
 
     public function networkMessage($networkStatus)
     {
-        return date('H:i:s') . ' ===> network connection status: ' . ($networkStatus ? 'connected' : 'not connected');
+        return 'network connection status ===> ' . ($networkStatus ? 'connected' : 'not connected');
     }
 
     public function webServerMessage($webServerStatus)
     {
-        return date('H:i:s') . ' ===> nginx status: ' . $webServerStatus;
+        return 'nginx status ===> ' . $webServerStatus;
     }
 }
