5aded715cd823da87dc7b75e439e965768b51ccb
mohsenabrishami <mohsenabrishami.ma@gmail.com>
2022-12-06 16:16:32 +0330

    update phpunit

--- tests/Models/ResourceLogTest.php
+++ tests/Models/ResourceLogTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Models;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use MohsenAbrishami\Stethoscope\Models\ResourceLog;
 use Tests\TestCase;
 
 class ResourceLogTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_resource_log_has_a_resource()
     {
         $resource = ResourceLog::factory()->create(['resource' => 'cpu']);
