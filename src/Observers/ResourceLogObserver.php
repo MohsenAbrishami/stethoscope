57037e9bb65280c9f7709bb0b85502bbf731948d
mohsenabrishami <mohsenabrishami.ma@gmail.com>
2022-12-28 16:13:59 +0330

    create observer and mail class

---
+++ src/Observers/ResourceLogObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use MohsenAbrishami\Stethoscope\Models\ResourceLog;
+
+class ResourceLogObserver
+{
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\ResourceLog  $resourceLog
+     * @return void
+     */
+    public function created(ResourceLog $resourceLog)
+    {
+        
+    }
+}
