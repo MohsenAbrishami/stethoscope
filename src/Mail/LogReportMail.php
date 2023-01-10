7cd5682a15e6a59b46ef83123f8eecc57cee87ee
mohsenabrishami <mohsenabrishami.ma@gmail.com>
2023-01-10 16:38:46 +0330

    Send email in queue

--- src/Mail/LogReportMail.php
+++ src/Mail/LogReportMail.php
@@ -3,10 +3,11 @@
 namespace MohsenAbrishami\Stethoscope\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class LogReportMail extends Mailable
+class LogReportMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
