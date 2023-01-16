d0efa432e330b8a58fe9029dd8de0ec20dbe2981
mohsen abrishami <mohsenabrishami.ma@gmail.com>
2023-01-16 10:51:21 +0330

    Create test to delete resource log file

--- tests/Features/CleanupCommandTest.php
+++ tests/Features/CleanupCommandTest.php
@@ -4,6 +4,7 @@ namespace Test\Features;
 
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Storage;
 use MohsenAbrishami\Stethoscope\Models\ResourceLog;
 use Tests\TestCase;
 
@@ -11,10 +12,10 @@ class CleanupCommandTest extends TestCase
 {
     use WithFaker;
 
-    public function test_delete_resource_logs_older_than_specified_number_of_days_in_config()
+    public function test_delete_old_resource_logs_from_database()
     {
         Config::set('stethoscope.drivers.log_record', 'database');
-        
+
         $oldLogId = ResourceLog::factory()->create([
             'created_at' => $this->faker->dateTimeBetween('-15 days', '-7 days')
         ])->id;
@@ -27,5 +28,23 @@ class CleanupCommandTest extends TestCase
 
         $this->assertDatabaseMissing('resource_logs', ['id' => $oldLogId]);
         $this->assertDatabaseHas('resource_logs', ['id' => $newLogId]);
+    }
+
+    public function test_delete_old_resource_log_files()
+    {
+        $oldLog = ResourceLog::factory()->create([
+            'created_at' => $this->faker->dateTimeBetween('-15 days', '-7 days')
+        ]);
+
+        $newLog = ResourceLog::factory()->create([
+            'created_at' => $this->faker->dateTimeBetween('-7 days')
+        ]);
+
+        $this->artisan('stethoscope:clean')->assertOk();
+
+        $path = config('stethoscope.log_file_storage.path');
+
+        Storage::assertMissing($path . $oldLog->created_at->toDateString());
+        Storage::assertExists($path . $newLog->created_at->toDateString());
     }
 }
