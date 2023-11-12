96bc399282385e0c96198bea8d48611c6c0014af
mohsen.abrishami <mohsenabrishami.ma@gmail.com>
2023-11-12 11:00:29 +0330

    Add loading to status

---
+++ resources/js/src/components/CurrentStatus/StatusItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="h-28 p-2 rounded-lg" :class="color">
+        <span>{{ title }}</span>
+        <div class="pt-4">
+            <span class="inline-block">{{ result }}</span>
+            <img
+                class="inline-block float-right w-12"
+                :src="icon"
+            >
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    color: {
+        type: String,
+        default: 'bg-blue-100',
+    },
+    title: {
+        type: String,
+        default: 'Loading...',
+    },
+    icon: {
+        type: String,
+        default: null,
+    },
+    result: {
+        type: String,
+        default: 'Loading...',
+    },
+})
+</script>
